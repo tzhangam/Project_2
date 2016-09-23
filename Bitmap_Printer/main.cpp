57359c282ebc5b9e6facda8e47ed017f3c4fae43
tzhangam <tzhangam@gmail.com>
2016-09-23 14:11:02 +0800

    Add a line

---
+++ Bitmap_Printer/main.cpp
@@ -0,0 +1,17 @@
+
+#include "Bitmap_Printer.h"
+#include <QtGui>
+#include <iostream>
+using namespace std;
+/*
+ Edit History:
+      This is the temporary program driver. 9-21-2016
+ */
+int main(int argc, char* argv[]){
+  QApplication app(argc, argv);
+  Bitmap_Printer gameboard;
+
+ 
+
+  return app.exec();
+}
